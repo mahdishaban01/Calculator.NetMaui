22dae6c643930bec4da49427bdef40c69cfc1261
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-04 11:44:53 +0330

    Add project files.

---
+++ Calculator/MVVM/ViewModels/CalcViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Dangl.Calculator;
+using PropertyChanged;
+using System.Windows.Input;
+
+namespace Calculator.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalcViewModel
+    {
+        public string Formula { get; set; }
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand =>
+            new Command((number) => { Formula += number; });
+
+        public ICommand ResetCommand =>
+            new Command(() =>
+            {
+                Result = "0";
+                Formula = "";
+            });
+
+        public ICommand BackspaceCommand =>
+            new Command(() =>
+            {
+                if (Formula.Length > 0)
+                    Formula = Formula.Substring(0, Formula.Length - 1);
+            });
+
+        public ICommand CalculateCommand =>
+            new Command(() =>
+            {
+                if (Formula.Length == 0)
+                    return;
+                var calculation =
+                    Dangl.Calculator.Calculator.Calculate(Formula);
+                Result = calculation.Result.ToString();
+            });
+    }
+}
